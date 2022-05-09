94dcdb0c8f5c358c83015ae827f6a335a3f1631f
Christian Crowle <christiancrowle@gmail.com>
2022-05-09 16:54:22 -0600

    fixed colours in drawrect
    drawRect previously took an unsigned char for a colour instead of a uint32_t, so it was only copying the last component of the colour
    (so everything was blue).

--- kernel/src/video/video.cpp
+++ kernel/src/video/video.cpp
@@ -17,13 +17,13 @@ void memcpy(uint8_t *source, uint8_t *dest, int nbytes)
 
 void initGUI()
 {
-    GlobalRenderer->ClearColor = 0x000000ff;
+    GlobalRenderer->ClearColor = 0x00ff0000;
     GlobalRenderer->Clear();
 
-    drawRect(0, 0, 40, 40, 0xaaaaaaaa);
+    drawRect(0, 850, 1600, 50, 0xffa4a4a4);
 }
 
-void drawRect(int startx, int starty, int width, int height, unsigned char VGA_COLOR)
+void drawRect(int startx, int starty, int width, int height, uint32_t VGA_COLOR)
 {
     int endx = startx + width;
     int endy = starty + height;
