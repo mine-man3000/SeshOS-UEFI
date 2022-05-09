1e59fd55091c98debf0d83589b0165aa3e49c63b
mine-man3000 <gdev0580@gmail.com>
2022-05-09 16:24:25 +0000

    fat 12 stuff

--- kernel/src/shell.cpp
+++ kernel/src/shell.cpp
@@ -113,5 +113,3 @@ void Shell::TestCMD(char* input)
 
 
 Shell *newShell;
-
-qemu-system-x86_64 -drive file=kernel/bin/CustomOS.img -m 1G -drive if=pflash,format=raw,unit=0,file="OVMF_CODE.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="OVMF_VARS.fd" -net none
